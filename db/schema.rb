16c97699799c4fc7841474ffe79210be110d89f9
James Jiang <jmendre@berkeley.edu>
2016-10-19 15:09:08 -0700

    stashing

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20160701062515) do
     t.datetime "created_at",                                             null: false
     t.datetime "updated_at",                                             null: false
     t.boolean  "admin"
-    t.datetime "last_sign_in_at",        default: '2016-10-06 03:33:44', null: false
+    t.datetime "last_sign_in_at",        default: '2016-10-14 15:04:50', null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

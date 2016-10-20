446a2720c93c1564a4c80375759d1929dc78c5cd
James Jiang <jmendre@berkeley.edu>
2016-10-19 21:11:05 -0700

    user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    describe "new user" do
+  	    it "create new user" do
+  	    	u = User.new
+  	    	u.email = "jack@gmail.com"
+  	    	u.delete
+  	    end
+    end
+end

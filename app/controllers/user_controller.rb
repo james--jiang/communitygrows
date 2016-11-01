52e53b2e8cce5dbe297a182825217689e07da2a4
Elmer Diaz <elmerd@berkeley.edu>
2016-11-01 01:25:21 +0000

    need to check for error case

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -31,13 +31,64 @@ class UserController < ActionController::Base
         # the model is already update through form_for 
         # so the question is what do we do here
         @user = User.find(params[:user_id])
+        # if @user.internal == 1
+        #     @user.internal = true
+        # end
+        # if @user.external == 1
+        #     @user.external = true
+        # end
+        # if @user.executive == 1
+        #     @user.executive = true
+        # end
+        
         if @user.update_attributes(user_params)
-            flash[:notice] = "feifejiejwzibirvsd"
+            print "start here"
+            print @user.internal
+            if @user.internal == 1
+                @user.internal = true
+                @user.save!
+            end
+            print @user.internal
+            if @user.external == 1
+                @user.external = true
+                @user.save!
+            end
+            if @user.executive == 1
+                @user.executive = true
+                @user.save!
+            end
+            if display_error(@user)
+                print "Huh"
+                flash[:notice] = "Please select at least your committee to receive emails from."
+            else
+                flash[:notice] = "Email Preferences were updated."
+            end
         else
             flash[:notice] = flash[:notice].to_a.concat @user.errors.full_messages
         end
         redirect_to user_credentials_path(@user.id)    
 
+    end
+    
+    def display_error(user)
+        print "ever"
+        if user.internal.nil? and user.external.nil? and user.executive.nil?
+            return true
+        elsif user.internal == 0 and user.external == 0 and user.executive == 0
+            return true
+        elsif user.internal.nil? and user.external == 0 and user.executive == 0
+            return true
+        elsif user.internal == 0 and user.external.nil? and user.executive == 0
+            return true
+        elsif user.internal == 0 and user.external == 0 and user.executive.nil?
+            return true
+        elsif user.internal == 0 and user.external.nil? and user.executive.nil?
+            return true
+        elsif user.internal.nil? and user.external == 0 and user.executive.nil?
+            return true
+        elsif user.internal.nil? and user.external.nil? and user.executive == 0
+            return true
+        end
     end
 
 end

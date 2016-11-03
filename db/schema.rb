50545731e0813cb059eda7cf82b40fa50a23deff
James Jiang <jmendre@berkeley.edu>
2016-11-02 23:02:51 -0700

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20161102231348) do
 
-
   create_table "announcements", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
@@ -77,7 +76,7 @@ ActiveRecord::Schema.define(version: 20161102231348) do
     t.datetime "created_at",                                             null: false
     t.datetime "updated_at",                                             null: false
     t.boolean  "admin"
-    t.datetime "last_sign_in_at",        default: '2016-11-02 16:12:50', null: false
+    t.datetime "last_sign_in_at",        default: '2016-10-14 15:04:50', null: false
     t.boolean  "internal"
     t.boolean  "external"
     t.boolean  "executive"

bc458aeda8f85ecab67f2eaa2ca4211f7fb84705
Nathan Pannell <tekkbot@gmail.com>
2016-11-08 23:41:07 -0800

    merged incorrectly. fixed

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -16,26 +16,26 @@ describe UserController do
         it 'should redirect to account info page on success' do
             user_params = {email: "tester@rspec.com", password: "communitygrowsrocks", password_confirmation: "communitygrowsrocks"}
             put :update, params: {user_id: users(:tester).id, user: user_params}
-            expect(response).to redirect_to(update_user_credentials_path(users(:tester).id))
+            expect(response).to redirect_to(update_user_credentials_path)
         end
         it 'should redirect to redirect to account info page on failure with failure message' do
             user_params = {email: "tester@rspec.com", password: "", password_confirmation: ""}
             put :update, params: {user_id: users(:tester).id, user: user_params}
-            expect(response).to redirect_to(update_user_credentials_path(users(:tester).id))
+            expect(response).to redirect_to(update_user_credentials_path)
         end
     end
     describe 'updateEmailPreferences' do
         it 'Should update email preferences' do
             user_params = {:email => "tester@rspec.com", :password => "communitygrowsrocks", :password_confirmation => "communitygrowsrocks",:internal=>"true"}
             post :updateEmailPreferences, params: {user_id: users(:tester).id, :user => user_params}
-            expect(response).to redirect_to(update_user_credentials_path(users(:tester).id))   
+            expect(response).to redirect_to(update_user_credentials_path)   
             expect(flash[:notice]).to match(/Your email preference settings have been updated./)      
 
         end
         it 'Should not update email preferences' do
             user_params = {email: "tester@rspec.com", password: "communitygrowsrocks", password_confirmation: "communitygrowsrocks", internal: "false", external: "false", executive: "false"}
             post :updateEmailPreferences, params: {user_id: users(:tester).id, user: user_params}
-            expect(response).to redirect_to(update_user_credentials_path(users(:tester).id))
+            expect(response).to redirect_to(update_user_credentials_path)
             expect(flash[:notice]).to match(/Please select at least your committee to receive emails from./)      
         end
     end

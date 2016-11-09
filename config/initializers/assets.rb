b343dc6ef1dfc225abfbf136d19ebc911614bb49
Nathan Pannell <tekkbot@gmail.com>
2016-11-09 00:27:54 -0800

    expired old assets. should recompile new assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '2.0'
 
 Rails.application.config.assets.precompile += %w( login.css ) #by Tony to compile login.css
 
